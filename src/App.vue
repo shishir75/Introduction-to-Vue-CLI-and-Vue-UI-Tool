fc2d787dda36e3da5ae3315912e67e0e5cb70b9d
shishir75 <shishir.srdr16@gmail.com>
2020-02-07 23:08:38 +0600

    Class and Style Bindings

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,64 @@
 <template>
   <div id="app">
     <tasks/>
+
+    <div :class="classObject">{{ value }}</div>
+    <div :style="{ fontSize: ( value*10 ) + 'px' }">{{ value }}</div>
+
+    <button type="button" @click="value++">+</button>
+    <button type="button" @click="value--">-</button>
+    <button type="button" @click="error = !error">Error</button>
+    <button type="button" @click="warning = !warning">Warning</button>
+
   </div>
 </template>
 
 <script>
-import Tasks from "./components/Tasks";
+  import Tasks from "./components/Tasks";
 
-export default {
-  name: 'app',
-  components: {
-    Tasks
+  export default {
+    name: 'app',
+    components: {
+      Tasks
+    },
+    data: function () {
+      return {
+        value: 1,
+        error: false,
+        warning: false,
+      }
+    },
+
+    computed: {
+      classObject: function () {
+        return {
+          'red-error' : this.value > 5,
+          'green-error' : this.value <= 5,
+          'yellow-warning' : this.warning
+        }
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .red-error {
+    background-color: darkred;
+  }
+
+  .green-error {
+    background-color: darkgreen;
+  }
+  .yellow-warning {
+    text-decoration: underline;
+  }
 </style>
