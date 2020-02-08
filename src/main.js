c283a2c37d4834ce82323f8252b1805be8a46971
shishir75 <shishir.srdr16@gmail.com>
2020-02-08 19:38:59 +0600

    Simple Routing From Scratch

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Tasks from "./components/Tasks";
+import HelloWorld from "./components/HelloWorld";
 
 Vue.config.productionTip = false
 
+const routes = {
+  '/' : App,
+  '/test' : Tasks,
+  '/hello' : HelloWorld
+};
+
+
 new Vue({
-  render: h => h(App),
+
+  data: {
+    currentRoute: window.location.pathname,
+  },
+
+  computed: {
+    currentComponent() {
+      return routes[this.currentRoute];
+    }
+  },
+
+  render: function (h) {
+    return h(this.currentComponent);
+  }
+
+
+
 }).$mount('#app')
